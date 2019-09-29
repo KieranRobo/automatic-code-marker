713c59e9c5a4777261192bd6a08f107d11470c20
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2019-09-29 21:21:10 +0100

    Testing

--- src/main/java/com/kieranrobertson/project/CodeCommander.java
+++ src/main/java/com/kieranrobertson/project/CodeCommander.java
@@ -4,6 +4,9 @@ import org.buildobjects.process.ProcBuilder;
 
 public class CodeCommander {
 
+    private final String testMainClass =
+            "public class Program { public static void main(String args[]) { System.out.println('This is a mounted program!');}}";
+
     private String submittedCode;
 
     public CodeCommander(String submittedCode) {
@@ -11,10 +14,13 @@ public class CodeCommander {
     }
 
     public void process() {
-        runShellCommand("echo","Hello World!");
+        runShellCommand("mkdir","/opt/app");
+        runShellCommand("echo","\""+ testMainClass + "\" > /opt/app/Program.java");
+        runShellCommand("docker", "run --rm -v /opt/app:/usr/src/app -w /usr/src/app openjdk:13 java Program");
     }
 
     private void runShellCommand(String cmd, String args) {
+        System.out.println("Running: " + cmd + " " + args);
         System.out.println(ProcBuilder.run(cmd, args.split(" ")));
     }
 
