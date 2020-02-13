a2d0d2e9e3af2fe2baccd46f34a86011439901b0
Kieran Robertson <kieranrobertson1@gmail.com>
2020-02-13 17:51:54 +0000

    Fixed some bootstrap stuff

--- frontend/src/components/challenges/solver/SubmissionResult.js
+++ frontend/src/components/challenges/solver/SubmissionResult.js
@@ -4,22 +4,22 @@ import { Alert } from 'react-bootstrap';
 const SubmissionResult = props => {
     if (props.result === "COMPILATION_ERROR") {
         return (
-        <div><Alert color="primary">
+        <div><Alert variant="dark">
             A compilation error occured with your submission.
         </Alert></div>);
     } else if (props.result === "UNKNOWN_ERROR") {
         return (
-        <div><Alert color="primary">
+        <div><Alert variant="dark">
             An unknown error has occured. Please consult application owner.
         </Alert></div>);
     } else if (props.result === "TEST_CASES_FAILURE") {
         return (
-        <div><Alert color="primary">
+        <Alert variant="danger">
             You're submission has failed one or more of the test cases.
-        </Alert></div>);
+        </Alert>);
     } else if (props.result === "SUCCESS") {
         return (
-        <div><Alert color="primary">
+        <div><Alert variant="success">
             You're submission has passed all test cases. Congratulations!
         </Alert></div>);
     } else {
